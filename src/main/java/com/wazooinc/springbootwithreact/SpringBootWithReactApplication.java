571bc83b823f4c4d8f0b115807cc8bd141550bbb
Erik Yuzwa <erikyuzwa@gmail.com>
2022-05-15 13:26:55 -0600

    initial commit

---
+++ src/main/java/com/wazooinc/springbootwithreact/SpringBootWithReactApplication.java
@@ -0,0 +1,13 @@
+package com.wazooinc.springbootwithreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wazooinc/springbootwithreact/SpringBootWithReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wazooinc.springbootwithreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
